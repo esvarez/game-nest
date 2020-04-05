1b5425ec4b4e82455e32715b0bc1abeea3583c84
Erick Suarez <ericksuarezdev@gmail.com>
2020-04-05 15:55:57 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+"# Roomies-units-service" 
---
+++ src/main/java/dev/ericksuarez/roomies/units/service/RoomiesUnitsServiceApplication.java
@@ -0,0 +1,13 @@
+package dev.ericksuarez.roomies.units.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoomiesUnitsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoomiesUnitsServiceApplication.class, args);
+	}
+
+}
